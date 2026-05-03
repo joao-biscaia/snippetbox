b36506d0b8fb5852971feebd76979ec911978398
joao-biscaia <joaopedro.jpbf@gmail.com>
2026-05-03 14:19:08 -0300

    adding `httprouter` package for more robust request handling

--- cmd/web/config/handlers.go
+++ cmd/web/config/handlers.go
@@ -7,70 +7,67 @@ import (
 	"os"
 	"snippetbox/internal/models"
 	"strconv"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func Home(app *Application) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			app.NotFound(w)
-			return
-		}
-
-		snippets, err := app.Snippets.Latest()
-		if err != nil {
-			app.ServerError(w, err)
-			return
-		}
-
-		data := app.NewTemplateData(r)
-		data.Snippets = snippets
-
-		app.Render(w, http.StatusOK, "home.tmpl.html", data)
+func (app *Application) Home(w http.ResponseWriter, r *http.Request) {
+	snippets, err := app.Snippets.Latest()
+	if err != nil {
+		app.ServerError(w, err)
+		return
 	}
+
+	data := app.NewTemplateData(r)
+	data.Snippets = snippets
+
+	app.Render(w, http.StatusOK, "home.tmpl.html", data)
 }
-func SnippetView(app *Application) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		id, err := strconv.Atoi(r.URL.Query().Get("id"))
-		if err != nil || id < 1 {
+func (app *Application) SnippetView(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil || id < 1 {
+		app.NotFound(w)
+		return
+	}
+
+	snippet, err := app.Snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
 			app.NotFound(w)
-			return
-		}
-
-		snippet, err := app.Snippets.Get(id)
-		if err != nil {
-			if errors.Is(err, models.ErrNoRecord) {
-				app.NotFound(w)
-			} else {
-				app.ServerError(w, err)
-			}
-			return
-		}
-
-		data := app.NewTemplateData(r)
-		data.Snippet = snippet
-
-		app.Render(w, http.StatusOK, "view.tmpl.html", data)
-	}
-}
-func SnippetCreate(app *Application) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			w.Header().Set("Allow", http.MethodPost)
-			app.ClientError(w, http.StatusMethodNotAllowed)
-			return
-		}
-
-		title := "test"
-		content := "test"
-		expires := 7
-
-		id, err := app.Snippets.Insert(title, content, expires)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error creating snippet: %v", err)
+		} else {
 			app.ServerError(w, err)
-			return
 		}
-
-		http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+		return
 	}
+
+	data := app.NewTemplateData(r)
+	data.Snippet = snippet
+
+	app.Render(w, http.StatusOK, "view.tmpl.html", data)
+}
+func (app *Application) SnippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		app.ClientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+
+	title := "test"
+	content := "test"
+	expires := 7
+
+	id, err := app.Snippets.Insert(title, content, expires)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error creating snippet: %v", err)
+		app.ServerError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view?id=%d", id), http.StatusSeeOther)
+}
+
+func (app *Application) SnippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display the form for creating a new snippet..."))
 }
--- cmd/web/config/routes.go
+++ cmd/web/config/routes.go
@@ -3,19 +3,26 @@ package config
 import (
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	"github.com/justinas/alice"
 )
 
 // The Routes() method returns a servemux containing our application Routes.
 func (app *Application) Routes(staticDir string) http.Handler {
-	mux := http.NewServeMux()
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.NotFound(w)
+	})
+
 	fileServer := http.FileServer(http.Dir(staticDir))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", Home(app))
-	mux.HandleFunc("/snippet/view", SnippetView(app))
-	mux.HandleFunc("/snippet/create", SnippetCreate(app))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
+
+	router.HandlerFunc(http.MethodGet, "/", app.Home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.SnippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.SnippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.SnippetCreatePost)
 
 	standard := alice.New(app.recoverPanic, app.logRequest, SecureHeaders)
 
-	return standard.Then(mux)
+	return standard.Then(router)
 }
